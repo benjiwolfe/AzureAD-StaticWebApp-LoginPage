ea33af47ce86ecc1cbc060748d80d305d073e5a0
Benjamin Wolfe <44454813+benjiwolfe@users.noreply.github.com>
2023-05-21 17:16:40 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const signUpBtn = document.getElementById('signIn');
 
 signUpBtn.addEventListener('click', () => {
-    window.location.replace("/.auth/login/aad?post_login_redirect_uri=https://zealous-grass-0ea7c0c03.2.azurestaticapps.net/authenticated/");
+    window.location.replace("/.auth/login/aad?post_login_redirect_uri=https://polite-bush-0dd2f3200.3.azurestaticapps.net/authenticated/");
 });
